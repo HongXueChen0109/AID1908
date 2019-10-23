d13ba5b1c2c5e66fdfc6ae4666eb72d59f6b6515
Tedu <2631317251@qq.com>
2019-10-23 10:35:36 +0800

    init commit

---
+++ exercise01.py
@@ -0,0 +1,42 @@
+"""
+    练习
+        编写出一个程序，从终端输入一个单词，打印出该单词的解释，如果
+        输入的单词找不到则打印"没有该单词"
+"""
+word = input("请输入单词")
+words = open("dict.txt","r")
+for item in words:
+    # if words in word and words == word:
+    wd = item.split(" ",1)[0]
+    if word == wd:
+        print(item)
+        break
+
+
+# """
+# 练习：
+# 编写一个程序，从终端输入一个单词，打印出该单词的解释，
+# 如果输入的单词找不到则打印 “没有该单词”
+# """
+#
+# word = input("Word:")  # 输入要查找的单词
+#
+# # 以读方式打开
+# f = open('dict.txt')
+#
+# # 每次获取一行
+# for line in f:
+#     wd = line.split(' ',1)[0]
+#     # 如果遍历到的单词已经大于目标单词，就不必查找了
+#     if wd > word:
+#         print("没有找到该单词")
+#         break
+#     # 比较是不是输入的单词
+#     elif word == wd:
+#         print(line)
+#         # print(line.split(' ',1)[1].strip())
+#         break
+# else:
+#     print("没有该单词")
+#
+# f.close()
